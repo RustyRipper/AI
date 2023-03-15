a2fed2cd5142fea4c1a73a8796eb74f563fe4b34
rustyripper <maciejmakarakontakt@gmail.com>
2023-03-15 09:40:21 +0100

    a* change, strange results :/

--- list1/List1/a_star_change.py
+++ list1/List1/a_star_change.py
@@ -12,17 +12,19 @@ def time_to_priority(time_str):
 
 
 def heuristic(goal: Node, next_one: Node):
-    return 10 * int(100 * (abs(goal.x - next_one.x) + abs(goal.y - next_one.y)))
+    return 100 * int(100 * (abs(goal.x - next_one.x) + abs(goal.y - next_one.y)))
 
 
 def a_star_change(graph, start, goal, start_time):
     frontier = PriorityQueue()
-    frontier.put((0, start))
+    frontier.put((time_to_priority(start_time), start))
     came_from = dict()
     cost_so_far = dict()
     line_so_far = dict()
+    cost_time = dict()
     came_from[start] = None
     cost_so_far[start] = 0
+    cost_time[start] = time_to_priority(start_time)
     line_so_far[start] = None
 
     how_many = 0
@@ -32,27 +34,34 @@ def a_star_change(graph, start, goal, start_time):
         if current == goal:
             break
 
-        current_time = time.strftime("%H:%M:%S", time.gmtime(cost_so_far[current]))
-        print(current_time)
+        #current_time = time.strftime("%H:%M:%S", time.gmtime(cost_time[current]))
+        #print(current_time)
+        if current == "Dubois" and came_from[current] == "Paulińska":
+            x= graph.neighbors_lines(current)
+            print()
 
         for next_one_edge in graph.neighbors_lines(current):
             how_many += 1
             new_cost_line = 0
             if line_so_far[current] != next_one_edge.line and current != start:
-                new_cost_line = 1000000
+                new_cost_line = cost_so_far[current] + 100000
             else:
-                line_so_far[current] = next_one_edge.line
+                new_cost_line = cost_so_far[current] + 1
             #new_cost = time_to_priority(graph.cost_time(current, next_one_edge.end_stop, current_time))
-            #print(next_one_edge.start_stop, next_one_edge.end_stop, next_one_edge.line, next_one_edge.edge_id)
-            if next_one_edge.end_stop not in cost_so_far or new_cost_line < cost_so_far[next_one_edge.end_stop]:
+
+
+            if (next_one_edge.end_stop not in cost_so_far or new_cost_line < cost_so_far[next_one_edge.end_stop]):
+
+                #cost_time[next_one_edge.end_stop] = new_cost
                 cost_so_far[next_one_edge.end_stop] = new_cost_line
                 line_so_far[next_one_edge.end_stop] = next_one_edge.line
-                priority = new_cost_line + heuristic(graph.get_node_by_name(goal),
-                                                graph.get_node_by_name(next_one_edge.end_stop))
+                priority = cost_so_far[next_one_edge.end_stop] + heuristic(graph.get_node_by_name(goal),
+                                                                graph.get_node_by_name(next_one_edge.end_stop))
                 print(priority)
                 frontier.put((priority, next_one_edge.end_stop))
                 came_from[next_one_edge.end_stop] = current
     print(how_many)
+    print('---')
     final_list = []
     new_key = goal
     while True:
